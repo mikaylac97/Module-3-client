b5b261fa7cf771849273a454fe0561ca37e6ae52
Mikayla Castro <mikaylacastro97@gmail.com>
2020-12-04 19:31:12 -0500

    frontend changes

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -52,18 +52,47 @@ export default class BookDetails extends Component {
         .catch(err => console.log(err))
     }
 
-    // removeFromHasRead = () => {
-    //     ACCOUNT_SERVICE
-    //         .removeBookFromHasReadList()
-    // }
-
+   
     render() {
         console.log(this.state.book)
         const { book } = this.state
         if(this.state.paramId !== this.props.match.params.bookId){ this.getDetails() }
         return (
-            <div>
-                <img src={book.image_url} alt='book-cover' />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-3'>
+                    <img src={book.image_url} alt='book-cover' />
+                    <button>
+                    <Link to={`/review/${book._id}`}>
+                    Write a Review
+                    </Link>
+                </button>
+                <button>
+                    <Link to={`/discuss/${book._id}`}>
+                    Start a Discussion
+                    </Link>
+                </button>
+                <button onClick={event => this.addToWantToRead(event)}>Add to want to read shelf</button>
+                <button onClick={event => this.addToHasRead(event)}>Add to has read shelf</button>
+                    </div>
+                    <div className='col-lg-9'>
+                    <h1>{book.title}</h1>
+                    <h3>{book.subtitle}</h3>
+                    <p>by: {book.authors}</p>
+                    <p>{book.description}</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div>
+                        {/* <h2>Reviews</h2> */}
+                        {/* {book.reviews.map(review => {
+                            return(
+                                <p>{review._id}</p>
+                            )
+                        })} */}
+                    </div>
+                </div>
+                {/* <img src={book.image_url} alt='book-cover' />
                 <button>
                     <Link to={`/review/${book._id}`}>
                     Write a Review
@@ -79,7 +108,7 @@ export default class BookDetails extends Component {
                     <h1>{book.title}</h1>
                     <h3>{book.subtitle}</h3>
                     <p>by: {book.authors}</p>
-                    <p>{book.description}</p>
+                    <p>{book.description}</p> */}
             </div>
         )
     }
